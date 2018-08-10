edb526624c5c9507fce65b47577cddf95bf16e98
RyunPyun <42006446+RyunPyun@users.noreply.github.com>
2018-08-10 09:30:48 -0500

    Mouse

---
+++ Mouse.cpp
@@ -0,0 +1,15 @@
+#include "Mouse.h"
+
+
+
+Mouse::Mouse()				//Kai
+{							//Kai
+	mouseTexture = IMG_LoadTexture(ren, "Mouse.png");//Kai
+	cursor = { 0,0,50,50 };	//Kai
+	tip = { 0,0,1,1 };		//Kai
+}							//Kai
+
+
+Mouse::~Mouse()				//Kai
+{							//Kai
+}							//Kai
